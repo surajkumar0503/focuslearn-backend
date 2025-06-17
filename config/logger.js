178d090d1d47bdc78e7c76c15de92793df6802f8
Suraj Kumar <surajkumar173a@mail.com>
2025-06-17 23:56:58 +0530

    Add logger config

---
+++ config/logger.js
@@ -0,0 +1,16 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log', level: 'debug' }),
+    new winston.transports.Console({ level: 'info' })
+  ]
+});
+
+module.exports = { logger };
