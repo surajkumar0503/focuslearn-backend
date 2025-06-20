14bc77c048b5f49522346c31196b099a268beaaf
Suraj Kumar <surajkumar173a@mail.com>
2025-06-21 01:59:25 +0530

    Fix arg in extractAudio fun

--- utils/audioExtractor.js
+++ utils/audioExtractor.js
@@ -107,7 +107,8 @@ async function extractAudio(videoUrl) {
       output: tempFile,
       userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
       referer: 'https://www.youtube.com/',
-      execPath: ytDlpPath
+    }, {
+      execPath: ytDlpPath 
     });
     logger.debug(`yt-dlp downloaded audio for ${videoId} to ${tempFile}`);
 
